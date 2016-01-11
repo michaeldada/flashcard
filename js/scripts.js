9fb681d00bd05d94e4b00ed499c33e5be56397b1
Michael Dada and Paul Hess <pauljacksonhess@gmail.com>
2016-01-11 15:36:00 -0800

    add content to flashcards

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+  $("#define1").hide();
+  $("#define2").hide();
+  $("#define3").hide();
+  $("#define4").hide();
+  $("#define5").hide();
+  $("#define6").hide();
+  $("#define7").hide();
+  $("#define8").hide();
+  $("#define9").hide();
+  $("#define10").hide();
+  $("#define11").hide();
+  $("#define12").hide();
+  $("#define13").hide();
+  $("#define14").hide();
+  $("#define15").hide();
+  $("#define16").hide();
+  $("#define17").hide();
+  $("#define18").hide();
+
+
+  $('#term1,#define1').click(function() {
+    $("#term1,#define1").toggle();
+  });
+  $('#term2,#define2').click(function() {
+    $("#term2,#define2").toggle();
+  });
+  $('#term3,#define3').click(function() {
+    $("#term3,#define3").toggle();
+  });
+  $('#term4,#define4').click(function() {
+    $("#term4,#define4").toggle();
+  });
+  $('#term5,#define5').click(function() {
+    $("#term5,#define5").toggle();
+  });
+  $('#term6,#define6').click(function() {
+    $("#term6,#define6").toggle();
+  });
+  $('#term7,#define7').click(function() {
+    $("#term7,#define7").toggle();
+  });
+  $('#term8,#define8').click(function() {
+    $("#term8,#define8").toggle();
+  });
+  $('#term9,#define9').click(function() {
+    $("#term9,#define9").toggle();
+  });
+  $('#term10,#define10').click(function() {
+    $("#term10,#define10").toggle();
+  });
+  $('#term11,#define11').click(function() {
+    $("#term11,#define11").toggle();
+  });
+  $('#term12,#define12').click(function() {
+    $("#term12,#define12").toggle();
+  });
+  $('#term13,#define13').click(function() {
+    $("#term13,#define13").toggle();
+  });
+  $('#term14,#define14').click(function() {
+    $("#term14,#define14").toggle();
+  });
+  $('#term15,#define15').click(function() {
+    $("#term15,#define15").toggle();
+  });
+  $('#term16,#define16').click(function() {
+    $("#term16,#define16").toggle();
+  });
+  $('#term17,#define17').click(function() {
+    $("#term17,#define17").toggle();
+  });
+  $('#term18,#define18').click(function() {
+    $("#term18,#define18").toggle();
+  });
+  
+
+});
